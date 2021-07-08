cebd1594859b4838b478a16b245db1ab46537111
rachelyedamkim <rachelyedamkim@users.noreply.github.com>
2021-07-08 00:19:11 -0700

    random test

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 let d = new Date();
 document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
 
-var startTime = Date.now();
-
-var interval = setInterval(function() {
-    var elapsedTime = Date.now() - startTime;
-    document.getElementById("timer").innerHTML = (elapsedTime / 1000).toFixed(3);
-}, 100);
+var things = ['Rock', 'Paper', 'Scissor'];
+var thing = things[Math.floor(Math.random()*things.length)];
+document.write('The computer chose: ' + thing);
