8579f4e517912e7d75404d6ac2ca2d277d4deb14
rachelyedamkim <rachelyedamkim@users.noreply.github.com>
2021-07-08 00:39:42 -0700

    facts

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let d = new Date();
 document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
 
-var things = ['Rock', 'Paper', 'Scissor'];
-var thing = things[Math.floor(Math.random()*things.length)];
-document.write('The computer chose: ' + thing);
+var facts = ['Rock', 'Paper', 'Scissor'];
+var fact = facts[Math.floor(Math.random()*facts.length)];
+document.write(fact);
